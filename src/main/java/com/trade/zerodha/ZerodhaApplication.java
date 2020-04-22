40ae279a575e32c158d684ab03f3135dc9158920
sharathparsam <codebase107@gmail.com>
2020-04-22 19:20:49 +0530

    base files

---
+++ src/main/java/com/trade/zerodha/ZerodhaApplication.java
@@ -0,0 +1,13 @@
+package com.trade.zerodha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerodhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerodhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trade/zerodha/ZerodhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.zerodha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
